724bd1ac6b9c29a59fd3edd75c557a957a557acf
MinYoop <myk430s@naver.com>
2018-11-27 11:55:25 +0900

    # WARNING: head commit changed in the meantime
    깃 연습

--- semiProjectUI/WebContent/js/sns.js
+++ semiProjectUI/WebContent/js/sns.js
@@ -24,23 +24,12 @@ FB.getLoginStatus(function(response) {
 });
 }
 */
-let fbgogo = function(fb){
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2&appId={앱아이디}&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-fb();};
 
   let fbstate = function(){ FB.getLoginStatus(function(response){
         	statusChangeCallback(response);
         });
   }
   
- Promise.resolve().then(fbgogo(fbstate));
 
       function statusChangeCallback(response){
 		  if(response.status==='connected'){
@@ -73,6 +62,7 @@ fb();};
 			
 			
 	
+			
 
 /**
  * 
